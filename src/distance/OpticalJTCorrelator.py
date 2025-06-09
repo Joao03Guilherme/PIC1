1a428a242fc3450a58fd31eed53c886c4e672a96
João Guilherme <joaommguilherme@tecnico.ulisboa.pt>
2025-06-09 12:35:30 +0100

    fixed JTC optical

--- src/distance/OpticalJTCorrelator.py
+++ src/distance/OpticalJTCorrelator.py
@@ -135,7 +135,6 @@ class OpticalJTCorrelator:
 
         self.slm = slm or ExulusSLM(**(exulus_kwargs or {}))
         self.cam = cam or UC480Controller()
-        cam.reset_roi()
 
         self.binary_input = binary_input
         self.binary_jps = binary_jps
